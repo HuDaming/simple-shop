86ba64c28eefd07787cd66db4c9161fc6c9fd65e
vagrant <vagrant@homestead>
2019-01-27 01:27:36 +0000

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 向容器中注入一个 alipay 的单例对象
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            // 线上环境写 Warning 日志，否则写 Dug 日志，方便开发调试
+            if (app()->environment() !== 'production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            // 创建 alipay 支付对象
+            return Pay::alipay($config);
+        });
+
+        // 注入一个 wechat_pay 的单例对象
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            return Pay::wechat($config);
+        });
     }
 }
